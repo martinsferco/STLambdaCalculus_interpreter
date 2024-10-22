b30a896af546d14944a97c8a5b8ac243cd3ca455
Martin Sferco <martinsferco.ms@gmail.com>
2024-10-22 16:35:18 -0300

    ex2 done

--- src/Simplytyped.hs
+++ src/Simplytyped.hs
@@ -47,6 +47,9 @@ conversionAux (LAbs x t lt) idxs  = let
 ----------------------------
 
 -- substituye una variable por un término en otro término
+-- cant abstraccione
+-- termino a sustituir
+-- donde sustituimos
 sub :: Int -> Term -> Term -> Term
 sub i t (Bound j) | i == j    = t
 sub _ _ (Bound j) | otherwise = Bound j
@@ -60,10 +63,23 @@ quote (VLam t f) = Lam t f
 
 -- evalúa un término en un entorno dado
 eval :: NameEnv Value Type -> Term -> Value
-eval = undefined
 
+--? Con las bound no hacemos nada?
 
+eval ne (Free x) = case Prelude.lookup x ne of
+                      Nothing     -> error "variable not in name enviroment"
+                      Just (v, t) -> v 
 
+eval ne ((Lam t f) :@: (Lam t' f')) = let tsub = sub 0 (Lam t' f') f
+                                      in  eval ne tsub 
+
+eval ne ((Lam t f) :@: t2) = let t2' = eval ne t2
+                             in eval ne ((Lam t f) :@: quote t2')
+
+eval ne (t1 :@: t2) = let t1' = eval ne t1
+                      in  eval ne (quote t1' :@: t2)
+
+eval _ (Lam t f) = VLam t f
 
 ----------------------
 --- type checker
@@ -100,10 +116,11 @@ notfunError t1 = err $ render (printType t1) ++ " no puede ser aplicado."
 notfoundError :: Name -> Either String Type
 notfoundError n = err $ show n ++ " no está definida."
 
+-- ?? Podemos usar el Prelude.
 -- infiere el tipo de un término a partir de un entorno local de variables y un entorno global
 infer' :: Context -> NameEnv Value Type -> Term -> Either String Type
 infer' c _ (Bound i) = ret (c !! i)
-infer' _ e (Free  n) = case Prelude.lookup n e of --? Preguntar si podemos poner Prelude.
+infer' _ e (Free  n) = case Prelude.lookup n e of 
   Nothing     -> notfoundError n
   Just (_, t) -> ret t
 infer' c e (t :@: u) = infer' c e t >>= \tt -> infer' c e u >>= \tu ->
